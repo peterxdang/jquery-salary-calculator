49e7d54a6572532edabf58c89db4a48f846ab877
Peter Dang <Peter.x.Dang@gmail.com>
2019-09-01 00:34:01 -0500

    adding all Annual salary

--- calculator.js
+++ calculator.js
@@ -17,12 +17,22 @@ function addInput () {
     let newLastName = $("#inputLastName").val();
     let newID = $("#inputId").val();
     let newTitle = $("#inputTitle").val();
-    let newAnnualSalary = $("#inputAnnualSalary").val(); 
+    let newAnnualSalary = $("#inputAnnualSalary").val();
+    let newAnnSalaryNum = Number(newAnnualSalary);
 
-    salaryArray.push(newAnnualSalary);
+    $("totalMonthly").removeClass('error');
+    if (newAnnSalaryNum !== Number(newAnnSalaryNum)) {
+        alert("Enter your Annual Salary as a number: no commas ( , )");
+        $("#inputAnnualSalary").val('');
+        return;
+    }
+    else {
+        salaryArray.push(Number(newAnnualSalary));
+    }
+
     console.log(salaryArray);
     $("#addNewEmployee2").append (
-        `<tr> 
+        `<tr id = "newEntry"> 
         <td>${newFirstName}</td>,
         <td>${newLastName}</td>,
         <td>${newID}</td>,
@@ -32,6 +42,25 @@ function addInput () {
         </tr>`
     );
     $("#deleteFeature").on('click', functionDelete );
+
+    function functionDelete () {
+        $("#newEntry").remove();
+    }
+    $("#totalMonthly").empty();
+    $("#totalMonthly").append(`<span>${addIt(salaryArray)}</span>`);
     
 
 }
+
+
+//[3,2,3,4]; 
+function addIt (here) {
+    let sum = 0;
+    for (let i=0; i<here.length; i++) {
+        sum = sum + here[i]
+    }
+    if (sum >20000 ) {
+        $("#totalMonthly").addClass(`error`);
+    }
+    return sum;
+}; 
